9fa3093b22b19fc10c998de2d03e2e8881365344
Akash-2006 <skumar169@lenovo.com>
2025-12-17 14:53:40 +0530

    added temperoary memmory for the agent

--- tests/test_agent.py
+++ tests/test_agent.py
@@ -3,3 +3,9 @@ from src.agent import Agent
 def test_agent_runs():
     agent = Agent()
     assert agent.run("hii") is not None
+
+def test_agent_memory():
+    agent = Agent()
+    assert len(agent.fetchMemory()) is 0
+    agent.run("hii")
+    assert len(agent.fetchMemory()) is not 1
